8accd1d8031d99fb560445f91035907eb025998d
letsgukto <114171540+letsgukto@users.noreply.github.com>
2022-10-16 13:05:21 +0900

    Update interact.js

--- interact.js
+++ interact.js
@@ -19,30 +19,50 @@ let windowTimers = [];
 let navBarTimers = [];
 
 windowHandler = () => {
-  const curY = window.scrollY + 58;
+  const curY = window.scrollY + 57;
   let coorToScroll;
-  if(curY < days[0].offsetTop - 58) {
+  if(curY < days[0].offsetTop - 57) {
     navBar.style.opacity = 0;
+    navBar.style.maxHeight = "0px";
   } else {
     navBar.style.opacity = 1;
+    navBar.style.maxHeight = "27px";
   }
   if(curY < days[0].offsetTop) {
     coorToScroll = NAV_BTN_WIDTH * (1.5) - window.screen.availWidth / 2;
+    navBtnFocused.classList.remove("focus");
+    navBtns[0].classList.add("focus");
+    navBtnFocused = navBtns[0];
   } else if(curY < days[1].offsetTop) {
     coorToScroll = NAV_BTN_WIDTH * (1.5) - window.screen.availWidth / 2
       + (curY - days[0].offsetTop) * (NAV_BTN_WIDTH) / (days[1].offsetTop - days[0].offsetTop);
+    navBtnFocused.classList.remove("focus");
+    navBtns[0].classList.add("focus");
+    navBtnFocused = navBtns[0];
   } else if(curY < days[2].offsetTop) {
     coorToScroll = NAV_BTN_WIDTH * (2.5) - window.screen.availWidth / 2
       + (curY - days[1].offsetTop) * (NAV_BTN_WIDTH) / (days[2].offsetTop - days[1].offsetTop);
+    navBtnFocused.classList.remove("focus");
+    navBtns[1].classList.add("focus");
+    navBtnFocused = navBtns[1];
   } else if(curY < days[3].offsetTop) {
     coorToScroll = NAV_BTN_WIDTH * (3.5) - window.screen.availWidth / 2
       + (curY - days[2].offsetTop) * (NAV_BTN_WIDTH) / (days[3].offsetTop - days[2].offsetTop);
+    navBtnFocused.classList.remove("focus");
+    navBtns[2].classList.add("focus");
+    navBtnFocused = navBtns[2];
   } else if(curY < days[4].offsetTop) {
     coorToScroll = NAV_BTN_WIDTH * (4.5) - window.screen.availWidth / 2
       + (curY - days[3].offsetTop) * (NAV_BTN_WIDTH) / (days[4].offsetTop - days[3].offsetTop);
+    navBtnFocused.classList.remove("focus");
+    navBtns[3].classList.add("focus");
+    navBtnFocused = navBtns[3];
   } else {
     coorToScroll = NAV_BTN_WIDTH * (5.5) - window.screen.availWidth / 2
       + (curY - days[4].offsetTop) * (NAV_BTN_WIDTH) / (wraper.offsetHeight - days[4].offsetTop);
+    navBtnFocused.classList.remove("focus");
+    navBtns[4].classList.add("focus");
+    navBtnFocused = navBtns[4];
   }
 
   navBar.removeEventListener("scroll", navBarHandler);
@@ -66,19 +86,36 @@ navBarHandler = () => {
   let coorToScroll = days[0].offsetTop;
   if(curX <= 0) {
     navBar.style.opacity = 0;
+    navBar.style.maxHeight = "0px";
   } else {
     navBar.style.opacity = 1;
+    navBar.style.maxHeight = "27px";
   }
   if(curX < NAV_BTN_WIDTH) {
     coorToScroll = days[0].offsetTop - 58 + curX * days[0].offsetHeight / NAV_BTN_WIDTH;
+    navBtnFocused.classList.remove("focus");
+    navBtns[0].classList.add("focus");
+    navBtnFocused = navBtns[0];
   } else if(curX < NAV_BTN_WIDTH * 2) {
     coorToScroll = days[1].offsetTop - 58 + (curX - NAV_BTN_WIDTH) * days[1].offsetHeight / NAV_BTN_WIDTH;
+    navBtnFocused.classList.remove("focus");
+    navBtns[1].classList.add("focus");
+    navBtnFocused = navBtns[1];
   } else if(curX < NAV_BTN_WIDTH * 3) {
     coorToScroll = days[2].offsetTop - 58 + (curX - NAV_BTN_WIDTH * 2) * days[2].offsetHeight / NAV_BTN_WIDTH;
+    navBtnFocused.classList.remove("focus");
+    navBtns[2].classList.add("focus");
+    navBtnFocused = navBtns[2];
   } else if(curX < NAV_BTN_WIDTH * 4) {
     coorToScroll = days[3].offsetTop - 58 + (curX - NAV_BTN_WIDTH * 3) * days[3].offsetHeight / NAV_BTN_WIDTH;
+    navBtnFocused.classList.remove("focus");
+    navBtns[3].classList.add("focus");
+    navBtnFocused = navBtns[3];
   } else if(curX < NAV_BTN_WIDTH * 5) {
     coorToScroll = days[4].offsetTop - 58 + (curX - NAV_BTN_WIDTH * 4) * days[4].offsetHeight / NAV_BTN_WIDTH;
+    navBtnFocused.classList.remove("focus");
+    navBtns[4].classList.add("focus");
+    navBtnFocused = navBtns[4];
   }
 
   window.removeEventListener("scroll", windowHandler);
@@ -116,7 +153,12 @@ const scrollToFocus = (index) => {
 scrollToFocus(0);
 
 window.onload = () => {
+  if(window.scrollY > days[0].offsetTop - 57) {
+    navBar.style.opacity = 1;
+    navBar.style.maxHeight = "27px";
+  }
   navBar.style.transitionDuration = ".6s";
+  navBar.style.transitionTimingFunction = "cubic-bezier(0.83, 0, 0.17, 1)";
   const thumImgPos = thumImg.getBoundingClientRect();
   if(-thumImgPos.y > 30) {
     thumImg.style.height = "0px";
@@ -144,11 +186,7 @@ navBtns.forEach((btn, index) => {
       btn.touchMoved = true;
   });
   btn.addEventListener("touchend", () => {
-    if(!btn.touchMoved) {
-      navBtnFocused.classList.remove("focus");
-      btn.classList.add("focus");
-      navBtnFocused = btn;
-  
+    if(!btn.touchMoved) {  
       scrollToFocus(index); 
     }
     btn.touchMoved = false;
